676d553f8ec4f927c1ba4f7f751087a45fe2e567
Fatiunis <fapalrz@unis.edu.gt>
2026-03-02 02:18:00 -0600

    commit para probar si funciona con sonar

--- HOSPITAL_SYSTEM_REACT/src/pages/admin/AdminHome.jsx
+++ HOSPITAL_SYSTEM_REACT/src/pages/admin/AdminHome.jsx
@@ -6,7 +6,7 @@ const AdminHome = () => {
   return (
     <div className="admin-home-container">
       <Container>
-        <h1 className="dashboard-title">Bienvenido, Administrador prueba droneeee</h1>
+        <h1 className="dashboard-title">Bienvenido, Administrador prueba drone dev sonar</h1>
         <p className="dashboard-text">
           Aquí puedes visualizar un resumen del sistema y gestionar todas las operaciones.
         </p>
